db8ede92e426314e74cf18c259ce9b0151a61fab
Batya Zamansky <batya@rufferto.(none)>
2012-11-10 12:45:39 -0500

    made a working recipe-ingredient-getter

--- GROUP_3/proof.py
+++ GROUP_3/proof.py
@@ -7,4 +7,29 @@ result = urllib2.urlopen(url).read()
 soup = BeautifulSoup(result)
 #print (soup.prettify())
 
-print soup.findAll(True, {'class':"lblIngAmount"})
+
+
+#getting all the ingredients of a given recipe
+def ingredients():
+    """
+    Gets a list of all the ingredients in a given recipe
+    """
+    a = soup.findAll(True, {'class':"ingredient-name"})
+    b = []
+    for item in a:
+        b.append(item.contents[0])
+    return b
+
+def ingredients2(recipe):
+    """
+    Gets a list of all the ingredients in any given recipe
+    """
+    soup = BeautifulSoup(urllib2.urlopen(recipe).read())
+    
+    a = soup.findAll(True, {'class':"ingredient-name"})
+    b = []
+    for item in a:
+        b.append(item.contents[0])
+    return b
+
+print ingredients2('http://allrecipes.com/recipe/brownie-frosting/detail.aspx?event8=1&prop24=SR_Title&e11=brownies&e8=Quick%20Search&event10=1&e7=Home%20Page')
