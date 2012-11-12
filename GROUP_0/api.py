271af2368bef8d4a162f767a8761fb8de6675d91
mengdilin <mengdilin95@gmail.com>
2012-11-11 16:38:51 -0800

    emusic api

--- GROUP_0/api.py
+++ GROUP_0/api.py
@@ -1,7 +1,8 @@
 import json
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import urllib2
 
+'''
 apikey="FILDTEOIK2HBORODV"
 
 url="http://developer.echonest.com/api/v4/song/search?api_key=FILDTEOIK2HBORODV&sort=song_hotttnesss-desc&bucket=song_hotttnesss"
@@ -19,4 +20,9 @@ for item in popular_songs:
     popularity.append(item["song_hotttnesss"])
     artist.append(item["artist_name"])
     song_title.append(item["title"])
-
+'''
+apikey="nxs4nu8b9u3ekhuv5gfynr7s"
+#3 different charts:album, track, artist
+track_url=http://api.emusic.com/track/charts?apiKey=nxs4nu8b9u3ekhuv5gfynr7s&format=json
+album_url=http://api.emusic.com/album/charts?apiKey=nxs4nu8b9u3ekhuv5gfynr7s&format=json
+artist_url=http://api.emusic.com/artist/charts?apiKey=nxs4nu8b9u3ekhuv5gfynr7s&format=json
