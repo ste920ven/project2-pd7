1d5093f23ef69e8f4d1245bf25d8b9269c0dd7b2
Brian Lam <iBriaan@gmail.com>
2012-11-11 19:33:35 -0500

    Shortened URL to read from to make future changes easier
    Changed code from
    print
    ingredients('http://allrecipes.com/recipe/brownie-frosting/detail.aspx?event8=1&prop24=SR_Title&e11=brownies&e8=Quick%20Search&event10=1&e7=Home%20Page
    
    to
    
    http://allrecipes.com/recipe/brownie-frosting/detail.aspx

--- GROUP_3/proof.py
+++ GROUP_3/proof.py
@@ -20,4 +20,14 @@ def ingredients(recipe):
         b.append(str(item.contents[0]))
     return b
 
+"""
 print ingredients('http://allrecipes.com/recipe/brownie-frosting/detail.aspx?event8=1&prop24=SR_Title&e11=brownies&e8=Quick%20Search&event10=1&e7=Home%20Page')
+I removed the unneccessary parameters from the URL to make it easier to use
+with future code, but I'll keep the long one commented in case we need it
+in the future. -Brian
+"""
+print ingredients("http://allrecipes.com/recipe/brownie-frosting/detail.aspx")
+
+#Tried functionality of shortened URL with other recipe, seems to work great
+print ingredients("http://allrecipes.com/recipe/vegan-cupcakes/detail.aspx")
+
