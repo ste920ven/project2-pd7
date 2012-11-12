9a96ac4c4317d92ba6ee5d984432a116b6e5d617
Mengdi Lin <mengdilin95@gmail.com>
2012-11-11 20:29:44 -0500

    changed api to emusic

--- GROUP_0/api.py
+++ GROUP_0/api.py
@@ -1,28 +1,37 @@
 import json
-#from bs4 import BeautifulSoup
 import urllib2
 
-'''
-apikey="FILDTEOIK2HBORODV"
-
-url="http://developer.echonest.com/api/v4/song/search?api_key=FILDTEOIK2HBORODV&sort=song_hotttnesss-desc&bucket=song_hotttnesss"
-
-request=urllib2.urlopen(url)
-result=json.loads(request.read())
-print result
-
-#somehow echonest api has some repeated data; when i searched for top songs, the api returns the same song title multiple times
-popular_songs=result["response"]["songs"]
-popularity=[]
-artist=[]
-song_title=[]
-for item in popular_songs:
-    popularity.append(item["song_hotttnesss"])
-    artist.append(item["artist_name"])
-    song_title.append(item["title"])
-'''
 apikey="nxs4nu8b9u3ekhuv5gfynr7s"
-#3 different charts:album, track, artist
-track_url=http://api.emusic.com/track/charts?apiKey=nxs4nu8b9u3ekhuv5gfynr7s&format=json
-album_url=http://api.emusic.com/album/charts?apiKey=nxs4nu8b9u3ekhuv5gfynr7s&format=json
-artist_url=http://api.emusic.com/artist/charts?apiKey=nxs4nu8b9u3ekhuv5gfynr7s&format=json
+song={}
+#3 different charts: album, track, artist, but i just used the chart for track
+track_url="http://api.emusic.com/track/charts?apiKey=nxs4nu8b9u3ekhuv5gfynr7s&format=json"
+album_url="http://api.emusic.com/album/charts?apiKey=nxs4nu8b9u3ekhuv5gfynr7s&format=json"
+artist_url="http://api.emusic.com/artist/charts?apiKey=nxs4nu8b9u3ekhuv5gfynr7s&format=json"
+
+#a dictionary called song that has song_title as key. Whenever you search for a song title, the dictionary will return song's album, artist, genre, label, and image of the album.
+def create_song_dict():
+    request=urllib2.urlopen(track_url)
+    result=json.loads(request.read())
+    for item in result["tracks"]:
+        song_title=item["name"]
+        album=item["album"]
+        song[song_title]={"album":album["name"],"artist":album["artist"]["name"],"genre":album["genre"]["name"],"label":album["label"]["name"],"image":album["image"],"id":album["id"]}
+
+'''
+def create_rating_dict():
+    rating_url="http://api.emusic.com/album/ratings?apiKey=nxs4nu8b9u3ekhuv5gfynr7s&format=json&albumId="
+    for key in song.keys():
+        new_url=rating_url+str(song[key]["id"])
+        print new_url
+        break
+        request=urllib2.urlopen(new_url)
+        result=json.loads(request.read())
+        song[key]["rating"]=result["album"]["ratings"]["communityRating"]["average"]
+    for key in song.keys():
+        print song[key]["ratings"]
+
+create_song_dict()
+create_rating_dict()
+
+'''
+
