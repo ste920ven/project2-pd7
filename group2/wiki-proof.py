a26dd34caf358400f218751bd454f7a859ff69be
jhaggerty95 <jhaggerty95@gmail.com>
2012-11-12 20:43:18 -0500

    get random title

--- group2/wiki-proof.py
+++ group2/wiki-proof.py
@@ -3,7 +3,7 @@ import json
 
 def getTitle():
     request=urllib2.urlopen("http://en.wikipedia.org/w/api.php?format=json=&action=query&list=random")
-    result json.loads(request.read())
+    result = json.loads(request.read())
     return request
 
 if __name__ == "__main__":
