262f5ecfa156785278a46d79b49d6d4c77e344d5
Sarah Duncan <duncanbdunkin@gmail.com>
2012-11-13 00:05:26 -0500

    blah

--- GROUP_7/apitest.py
+++ GROUP_7/apitest.py
@@ -4,12 +4,14 @@ from bs4 import BeautifulSoup
 
 #place/nearbysearch location=-33.8670522,151.1957362&radius=500
 
-def test(address):
+def test(address, tag):
     geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false"%(address)
     maps_request = urllib.urlopen(geocode_url)
     geo_results = json.loads(maps_request.read())
-    print("google maps: \n")
+    print("\n google maps: ")
     print geo_results['results'][0]['formatted_address']
+    print("\n")
 
 a = "345 Chambers Street, New York"
-test(a)
+t = "park"
+test(a, t)
