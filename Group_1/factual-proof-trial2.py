69fd85a9e3eecb846e8e1326ac07a38ce63ab220
David Kurkovskiy <brainmonkey95@gmail.com>
2012-11-12 20:44:21 -0500

    DK

---
+++ Group_1/factual-proof-trial2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+#based on Mr. Z's NYT rest code
+
+import urllib
+import json
+import sys
+
+class Rester:
+    def __init__(self,url):
+        self.url = url
+
+    def call(self,q):
+        urlstring = "%s?%s"%(self.url,q)
+        print urlstring
+        
+        request = urllib.urlopen(urlstring)
+        #result = request.read();
+        result = json.loads(request.read())
+        return result
+
+
+r = Rester("http://api.v3/factual.com/t/restaurants-us")
+qstring = "q=Sushi,New York&KEY=drr6uQjOApDhEhzIbCVd63B70xUm71fIIr04CIxN"
+
+result = r.call(qstring) # remember rester converts from json
+
+for rest in result['response']['data']:
+    print rest['name']
+    print
