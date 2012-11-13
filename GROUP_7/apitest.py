3dbf333b09a1fcbaf821389378f3fac8629b58a6
Tran <tiffanyphan@ubuntu.(none)>
2012-11-13 00:03:40 -0500

    searchnearby

--- GROUP_7/apitest.py
+++ GROUP_7/apitest.py
@@ -13,3 +13,17 @@ def test(address):
 
 a = "345 Chambers Street, New York"
 test(a)
+
+
+def test(loc,radius,key):
+    google_maps_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%d&radius=%d&sensor=false&key=%s"%(loc,radius,key)
+    maps_request = urllib.urlopen(google_maps_url)
+    result = json.loads(maps_request.read())
+    return result
+
+key='AIzaSyCM_M56XMiApQyGP8gYGVafJmix15AcUng'
+loc="-33.8670522,151.1957362"
+radius="300"
+
+test(loc,radius,key)
+
