459604655b6ace64802355fce7e48d5f6da741ae
Sarah Duncan <duncanbdunkin@gmail.com>
2012-11-12 23:22:02 -0500

    got geocode api to work

--- GROUP_7/apitest.py
+++ GROUP_7/apitest.py
@@ -6,10 +6,11 @@ from bs4 import BeautifulSoup
 
 def test(address):
     geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false"%(address)
-    maps_request = urllib.urlopen(google_maps_url)
+    maps_request = urllib.urlopen(geocode_url)
     results = json.loads(maps_request.read())
     print("google maps: \n")
     print results['results'][0]['formatted_address']
+    pop_dens_url="http://server.arcgisonline.com/ArcGIS/rest/services/Demographics/USA_Population_Density/MapServer?"
 
 a = "722 East 22nd Street, Brooklyn"
 test(a)
