996954c8d9f310b15c57f14f07da68e2d4bb3e1c
jhaggerty95 <jhaggerty95@gmail.com>
2012-11-12 21:06:43 -0500

    get random quote

--- group2/wiki-proof.py
+++ group2/wiki-proof.py
@@ -4,7 +4,11 @@ import json
 def getTitle():
     request=urllib2.urlopen("http://en.wikipedia.org/w/api.php?format=json=&action=query&list=random&rnnamespace=0")
     result = json.loads(request.read())
-    return request
+    return result
 
-if __name__ == "__main__":
-    getTitle()
+def getQuote():
+     request=urllib2.urlopen("http://www.iheartquotes.com/api/v1/random?format=json&max_lines=2")
+     result = json.loads(request.read())
+     return result
+
+
