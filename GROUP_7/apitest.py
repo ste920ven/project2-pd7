ae73b21186b2c06389cfc6e282ec773e72badec1
Sarah Duncan <duncanbdunkin@gmail.com>
2012-11-12 23:13:03 -0500

    got geocode api to work

--- GROUP_7/apitest.py
+++ GROUP_7/apitest.py
@@ -5,10 +5,11 @@ from bs4 import BeautifulSoup
 #place/nearbysearch location=-33.8670522,151.1957362&radius=500
 
 def test(address):
-    google_maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false"%(address)
+    geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false"%(address)
     maps_request = urllib.urlopen(google_maps_url)
     results = json.loads(maps_request.read())
     print("google maps: \n")
     print results['results'][0]['formatted_address']
+
 a = "722 East 22nd Street, Brooklyn"
 test(a)
