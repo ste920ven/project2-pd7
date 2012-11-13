cf5d0ef8b6f5f289fe444b6cc299524a61651449
Sarah Duncan <duncanbdunkin@gmail.com>
2012-11-12 21:55:39 -0500

    working on apitest.py

--- GROUP_7/apitest.py
+++ GROUP_7/apitest.py
@@ -1,9 +1,13 @@
 import json
 import urllib
+from bs4 import BeautifulSoup
+
+google_maps_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&sensor=false&key=AIzaSyCM_M56XMiApQyGP8gYGVafJmix15AcUng"
 
 def test():
-    request = urllib.urlopen("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&sensor=false&key=AIzaSyCM_M56XMiApQyGP8gYGVafJmix15AcUng")
-    result = json.load(request.read())
-    print result
+    maps_request = urllib.urlopen(google_maps_url).read()
+    soup = BeautifulSoup(maps_request)
+    print("google maps: \n")
+    print soup
 
 test()
