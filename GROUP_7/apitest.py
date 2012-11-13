95331c6ead99589027581317943621c69808a113
Sarah Duncan <duncanbdunkin@gmail.com>
2012-11-13 00:12:31 -0500

    updated

--- GROUP_7/apitest.py
+++ GROUP_7/apitest.py
@@ -20,11 +20,12 @@ def test2(loc,radius,key,tag):
     google_maps_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s&radius=%s&sensor=false&key=%s&types=%s"%(loc,radius,key,tag)
     maps_request = urllib.urlopen(google_maps_url)
     result = json.loads(maps_request.read())
+    print("search nearby: ")
     print result['results']
 
 key='AIzaSyCM_M56XMiApQyGP8gYGVafJmix15AcUng'
 loc="-33.8670522,151.1957362"
-radius="300"
+radius="30000"
 tag="park"
 
 test2(loc,radius,key,tag)
