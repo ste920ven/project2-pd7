b3f9590577fcf00cffa9eeec35606b706bb42bf1
Sarah Duncan <duncanbdunkin@gmail.com>
2012-11-12 23:11:03 -0500

    got geocode api to work

--- GROUP_7/apitest.py
+++ GROUP_7/apitest.py
@@ -2,12 +2,13 @@ import json
 import urllib
 from bs4 import BeautifulSoup
 
-def test(key):
-    google_maps_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&sensor=false&key=%s"%(key)
-    maps_request = urllib.urlopen(google_maps_url).read()
-    soup = BeautifulSoup(maps_request)
-    print("google maps: \n")
-    print soup
+#place/nearbysearch location=-33.8670522,151.1957362&radius=500
 
-key="AIzaSyCM_M56XMiApQyGP8gYGVafJmix15AcUng"
-test(key)
+def test(address):
+    google_maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false"%(address)
+    maps_request = urllib.urlopen(google_maps_url)
+    results = json.loads(maps_request.read())
+    print("google maps: \n")
+    print results['results'][0]['formatted_address']
+a = "722 East 22nd Street, Brooklyn"
+test(a)
