d5f1672afc7f2c81a6a6c74687bc02e979307580
Zachary <Oneman2feet@gmail.com>
2012-11-13 13:12:03 -0500

    hello i did stuff

--- four-fifths/app.py
+++ four-fifths/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
+import extractor
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('home.html')
+    news     = extractor.getNews()
+    schedule = extractor.getSchedule()
+    return render_template('home.html',
+                           news=news,
+                           schedule=schedule)
 
 if __name__ == '__main__':
     app.debug = True
