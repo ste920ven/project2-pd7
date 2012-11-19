5343ad579a15f5adfe95707a94278e52fc7bb67b
Ivan <ivansmirnov13@gmail.com>
2012-11-18 19:16:59 -0500

    started db

---
+++ GROUP_0/database.py
@@ -0,0 +1,50 @@
+usernames = []
+passwords = []
+#songRatings = []
+#albumRatings = []
+songList = []
+albumList = []
+
+
+def saveUsername():
+
+def savePassword():
+
+
+#We will take input on from the register page and "save" it by putting the username and password in their respective lists
+
+
+def verifyLogin(username, password):
+    if username in usernames:
+        if password in passwords:
+            return true
+        else
+        return false
+
+
+#basic login method that verifies input username and password
+
+
+def addSong(song):
+    songList.append(song)
+
+def addAlbum(album):
+    albumList.append(album)
+
+def addSongRating(song):
+    
+def addAlbumRating(album):
+        
+
+def getSongRating(song):
+
+def getAlbumRating(album):
+
+#will return the rating of the song/album
+    
+
+
+
+
+        
+    
