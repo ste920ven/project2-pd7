dd45af2e3d408295f0e07492f96e298711824810
Raymond Zeng <raymondzeng@live.com>
2012-11-24 03:43:19 -0500

    n

--- GROUP_7/scripts.js
+++ GROUP_7/scripts.js
@@ -3,18 +3,48 @@ var geocoder;
 var directionDisplay;
 var directionsService = new google.maps.DirectionsService();
 
+var minZoomLevel = 10;
+
+//commented out lines limit map panning to nyc; but interferes with geocode and directions if locations out of nyc
+//var strictBounds = new google.maps.LatLngBounds(
+//    new google.maps.LatLng(40.473069,-74.31015), 
+//    new google.maps.LatLng(40.922852,-73.693542)
+//);
+
 function initialize() {
     geocoder = new google.maps.Geocoder();
     directionsDisplay = new google.maps.DirectionsRenderer();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var latlng = new google.maps.LatLng(40.720461,-74.013519);
     var mapOptions = {
-	zoom: 8,
-	center: new google.maps.LatLng(-34.397, 150.644),
+	zoom: minZoomLevel,
+	center: new google.maps.LatLng(40.720461,-74.013519),
 	mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById('map_canvas'),
 			      mapOptions);
     directionsDisplay.setMap(map);
+
+    google.maps.event.addListener(map, 'zoom_changed', function() {
+	if(map.zoom < minZoomLevel) map.setZoom(minZoomLevel);
+    });
+
+ /*   google.maps.event.addListener(map, 'dragend', function() {
+	if (strictBounds.contains(map.getCenter())) return;
+	var c = map.getCenter(),
+        x = c.lng(),
+        y = c.lat(),
+        maxX = strictBounds.getNorthEast().lng(),
+        maxY = strictBounds.getNorthEast().lat(),
+        minX = strictBounds.getSouthWest().lng(),
+        minY = strictBounds.getSouthWest().lat();
+	
+	if (x < minX) x = minX;
+	if (x > maxX) x = maxX;
+	if (y < minY) y = minY;
+	if (y > maxY) y = maxY;
+	
+	map.setCenter(new google.maps.LatLng(y, x));
+    });*/
 }
 
 function codeAddress() {
@@ -47,3 +77,4 @@ function calcRoute() {
         }
     });
 }
+
