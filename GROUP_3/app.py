ec15722ace6f38ec462097961947e01b3cd9676e
Dexter Tong <schoolpurposes@gmail.com>
2012-11-23 21:53:39 -0500

    submit button working

--- GROUP_3/app.py
+++ GROUP_3/app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 @app.route("/", methods = ["GET", "POST"])
 def home():
     if request.method == 'POST':
+        #print "cardamom"
+        #print request.form['foodname']
         foodname = request.form['foodname']
         return findprice()
     else:
@@ -14,6 +16,7 @@ def home():
 
 @app.route("/findprice", methods = ["GET", "POST"])
 def findprice():
+    print "SUCESS"
     return render_template("pricer.html")
 
 if __name__ == '__main__':
