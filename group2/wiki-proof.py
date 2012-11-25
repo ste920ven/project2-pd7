a8100c6382f01a06d6497b699a686b31660ebec9
jhaggerty95 <jhaggerty95@gmail.com>
2012-11-25 17:53:05 -0500

    Because

--- group2/wiki-proof.py
+++ group2/wiki-proof.py
@@ -1,14 +1,19 @@
 import urllib2
-import json
+import simplejson
 
 def getTitle():
-    request=urllib2.urlopen("http://en.wikipedia.org/w/api.php?format=json=&action=query&list=random&rnnamespace=0")
-    result = json.loads(request.read())
+    request=urllib2.urlopen("http://en.wikipedia.org/w/api.php?action=query&format=json&generator=random&redirects=&grnnamespace=0&grnlimit=1")
+    result = simplejson.loads(request.read())
+    print result
     return result
 
 def getQuote():
      request=urllib2.urlopen("http://www.iheartquotes.com/api/v1/random?format=json&max_lines=2")
-     result = json.loads(request.read())
+     result = simplejson.loads(request.read())
+     print result
      return result
 
-
+if __name__ == "__main__":
+    getTitle()
+    print '\n'
+    getQuote()
