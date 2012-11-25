ecd15e7af801251b64eeb888a2e4b55c44b6d96d
Zachary Zee <Oneman2feet@gmail.com>
2012-11-25 15:13:07 -0500

    returned to normal non-debugging mode

--- four-fifths/app.py
+++ four-fifths/app.py
@@ -14,8 +14,8 @@ def main():
     return render_template('home.html',
                            news=news,
                            schedule=schedule,
-                           bellDay="Homeroom",#bellDay,
-                           gymDay="A",#gymDay,
+                           bellDay=bellDay,
+                           gymDay=gymDay,
                            date=date)
 
 if __name__ == '__main__':
