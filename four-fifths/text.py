4b3e207e7efb031e6aed0ccde7c43cee4a8506fc
Jack Stevenson <jskestrel@gmail.com>
2012-11-27 23:38:43 -0500

    on a howard roll

--- four-fifths/text.py
+++ four-fifths/text.py
@@ -29,9 +29,6 @@ def text():
 @app.route('/incomingVoice', methods=['POST'])
 def incomingVoice():
     resp = twiml.Response()
-#note: spelled out "fizz" because it probably can't pronounce "phys"
-#    welcome = "Welcome to the Stuyvesant information hotline. Press one for today's schedule and fizz ed cycle. Press two for the weather at Stuyvesant today."
- #   resp.gather(numDigits=1, action="/scheduleweather").say(welcome)
     audio = []
     audio.append("welcome-1.mp3")
     audio.append("press-1.mp3")
@@ -43,13 +40,13 @@ def incomingVoice():
     for each in audio:
         url = url_for("static", filename=("audio/%s"%(each)))
         gather.play(url)
-    print str(resp)
     return str(resp)
 
 @app.route('/scheduleweather', methods=['POST'])
 def schedule():
     digit = request.form['Digits']
     resp = twiml.Response()
+    audio = []
 #---pressed 1: schedule---
     if int(digit) == 1 :
         print "1 case: schedule"
@@ -57,18 +54,15 @@ def schedule():
         schedule = extractor.getSchedule(data[1], data[2])
         bellDay = extractor.getBellDay(schedule)
         if bellDay == "Closed" :
-            message = "School is closed today."
+            audio.append("closed.mp3")
         elif bellDay == "Weekend" :
-            message = "It's a weekend. There is no school today."
+# pending weekend recording
+            audio.append("closed.mp3")
         elif bellDay == "Unknown" :
-            message = "We don't have today's schedule. How embarrassing."
+            audio.append("unknown.mp3")
         else :
-#'a' or 'an' depending on the next word:
-#B1/B2 ('a') or A1/A2/Unknown ('an')
             gymDay = extractor.getGymDay(schedule)
-            if gymDay[0] == "B" : article = "a"
-            else : article = "an"
-            message = "Today is a %s schedule. Today is %s %s day."%(bellDay, article, gymDay)
+            audio.append("%s.mp3"%(gymDay))            
 #---pressed 2: weather---
     elif int(digit) == 2 :
         print "2 case: weather"
@@ -76,9 +70,12 @@ def schedule():
 #---pressed another button
     else :
         print "Not 1 or 2. Bad user."
-        message = "You didn't press one or two. Bad user."
-    message += " Press any key to go back."
-    resp.gather(numDigits=1, action="/incomingVoice").say(message)
+        audio.append("baduser.mp3")
+    audio.append("back.mp3")
+    gather = resp.gather(numDigits=1, action="/incomingVoice")
+    for each in audio:
+        url = url_for("static", filename=("audio/%s"%(each)))
+        gather.play(url)
     return str(resp)
 
 if __name__ == '__main__':
