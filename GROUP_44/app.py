5ae6509ff6b55f6c4a806242f6a4d6374fd7d5cc
haydenkh <hayden.kh@gmail.com>
2012-11-27 20:25:59 -0500

    app.py barely works

--- GROUP_44/app.py
+++ GROUP_44/app.py
@@ -4,9 +4,17 @@ from pymongo import connection
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def home():
-    return redirect("/survey")
+    if request.method == "GET":
+        print "get!!"
+        return render_template("survey.html")
+    else:
+        button=request.form.get('button',"")
+        if button == 'Save!':
+            print "post works!!"
+            return render_template("survey.html")
+
 
 if __name__ == "__main__":
     app.run(debug = True)
