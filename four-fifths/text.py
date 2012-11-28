25b03b2ad2afe60431ad4dd94dc6fb985046aa83
Jack Stevenson <jskestrel@gmail.com>
2012-11-27 23:03:14 -0500

    small files for quick playback?

--- four-fifths/text.py
+++ four-fifths/text.py
@@ -33,9 +33,9 @@ def incomingVoice():
 #    welcome = "Welcome to the Stuyvesant information hotline. Press one for today's schedule and fizz ed cycle. Press two for the weather at Stuyvesant today."
  #   resp.gather(numDigits=1, action="/scheduleweather").say(welcome)
     audio = []
-    audio.append("welcome-1.mp3")
-    audio.append("press-1.wav")
-    audio.append("press-2.mp3")
+    audio.append("A.mp3")
+    audio.append("A1.mp3")
+    audio.append("A2.mp3")
 #don't have MTA or info pages just yet
 #  audio.append("press-3.mp3")
 #  audio.append("press-4.mp3")
