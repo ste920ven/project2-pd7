496707dee7b97838aeda86f54931cff0722b25d4
Zachary <Oneman2feet@gmail.com>
2012-11-29 21:19:54 -0500

    responsive statusbar

--- four-fifths/static/sidebar.js
+++ four-fifths/static/sidebar.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
 	$('div#sidebar').animate({right:"-30%"},1200,function(){
 	    $('div#sidebar div#toggle').html("&laquo;").animate({left:"-70px"},500).addClass('hidden');
        	});
-	$('div#main').animate({padding:"4% 12.5%",width:"75%"},1200);
+	$('div#main').animate({padding:"4% 12.5%",width:"75%"},{duration: 1200, step: resize});
     },function(){
 	$('div#sidebar div#toggle').animate({left:"0px"},500,function(){
 	    $(this).html("&raquo;");
  	    $('div#sidebar').animate({right:"0%"},1200);
-	    $('div#main').animate({padding:"4%",width:"62%"},1200);
+	    $('div#main').animate({padding:"4%",width:"62%"},{duration: 1200, step: resize});
 	}).removeClass('hidden');
     });
 });
