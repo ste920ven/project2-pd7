cb2db64aa0dc0bc1baf97175e0618c4ff8fc2993
Leopold Spohngellert <leopoldsg94@gmail.com>
2012-12-01 00:24:05 -0500

    woo

--- GROUP_44/app.py
+++ GROUP_44/app.py
@@ -1,16 +1,32 @@
 from flask import Flask,url_for,redirect,flash,session,escape,request,render_template
 from pymongo import connection
+import login
+#import espn
+
 
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 
 @app.route("/",methods=["GET","POST"])
+def login():
+    global username
+    if request.method == "GET":
+        return render_template("user.html")
+
+    if request.method == "POST":
+       # if request.form["button"] == "Submit": #creating a username
+           #username = str(request.form['username'])
+           #login.newUser(username)
+        return redirect(url_for('home'))
+
+
+@app.route("/home", methods= ["GET","POST"])
 def home():
     if request.method == "GET":
         print "get!!"
         return render_template("survey.html")
-    else:
-        button=request.form.get('button',"")
+    
+    if  button==request.form.get('button',""):
         print "post1"
         if button == 'Save!':
             print "post works!!"
@@ -24,7 +40,6 @@ def home():
                 ##means it was selected
                 return Action
             return render_template("results.html")
-
 
 if __name__ == "__main__":
     app.run(debug = True)
