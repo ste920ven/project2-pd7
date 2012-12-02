6e7b7b67bbf8221bee0e1892040088dc232e16fd
Steven <steven@ubuntu.ubuntu-domain>
2012-12-02 02:56:29 -0500

    editted

--- Group_1/app.py
+++ Group_1/app.py
@@ -1,21 +1,3 @@
-from flask import Flask, request, redirect
-import twilio.twiml
-import factual_search
-
-app = Flask(__name__)
- 
-@app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
-    text = request.values.get('Body', None)
-    result = factual_search.getSearchString(text)
-    resp = twilio.twiml.Response()
-    resp.sms(result)
-    #resp.sms("Hello")
-    return str(resp)
- 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7201, debug=False)
 from flask import Flask, request, redirect, render_template, session,url_for,flash
 import twilio.twiml
 import factual_search
