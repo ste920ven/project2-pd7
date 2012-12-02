c0a0b4688eaa1334760332249c132f7cb779e0e3
Sarah Duncan <duncanbdunkin@gmail.com>
2012-12-02 00:14:01 -0500

    api.py now prints example facility names

--- GROUP_7/api.py
+++ GROUP_7/api.py
@@ -34,34 +34,6 @@ def durationTo(origins, destinations, mode):
     print "\n The time it takes to travel  between %s and %s is" % (original, destination)
     print result['rows'][0]['elements'][0]['duration']['text']
 
-#we need a dropdown menu that restricts the facilities the users can choose from. then in util.py include a function that can translate that chosen facility into its encoded input value (which will be inputted as category for the function below)
-#this function will return all the facilities in NY that are of the selected category. i'm trying to get it to print out in order of closest to furthest distance away from the user's origin
-def getFacilityList(origin,category, mode): 
-    facilityList_url = "http://www.nyc.gov/portal/apps/311_contentapi/facilities/%s.json"%(category)
-    facilityList_request = urllib.urlopen(facilityList_url)
-    facilityList_results = json.loads(facilityList_request.read())
-    #facilities = []
-    #fac_in_order = []
-    #dist_in_order = []
-    print facilityList_results['facility_name'] #this won't work?!?!!?! 
-    """facilities.append(facilityList_results["facility_name"])
-    #for facility in facilities:
-        #fac_add = getAddress(facility)
-        #distance = distanceFrom(origin, fac_add, mode)
-        #dist_in_order.append(distance)
-        if len(fac_in_order) == 0: 
-            fac_in_order.append(facility)
-        else:
-            if this.distance > fac_in_order[0].distance:
-                fac_in_order.append(facility)
-            else:
-                for a in fac_in_order:
-                    if this.distance <= a.distance:
-                        fac_in_order.insert((fac_in_order.index(a)), facility)
-    dist_in_order.sort()
-    print fac_in_order
-    print dist_in_order"""
-
 def isPOI(origins):
     geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false"%(origins)
     maps_request = urllib.urlopen(geocode_url)
@@ -77,12 +49,9 @@ destination = "97 Warren Street"
 poi = "Empire State Building"
 #somehow we have to restrict the modes they can choose from. cause certain inputs like "walking" "driving" and "car" (i believe) work, but "foot" doesn't. so maybe do a dropdown menu somewhere in utils/app.py. i'll change the variable values once we have a more solid thing to work off of
 m = "walking" 
-facility = "Beach"
-
 getAddress(original)
 getLatLong(original)
 distanceFrom(original, destination, m)
 durationTo(original, destination, m)
-getFacilityList(facility)
 print "\n Is %s a POI? : %s" %(original, isPOI(original))
 print "\n Is %s a POI? : %s" %(poi, isPOI(poi))
--- GROUP_7/static/scripts.js
+++ GROUP_7/static/scripts.js
@@ -1,64 +1,16 @@
-$(document).ready(function(){
-    $("#trigger").click(function(){
-	$("#panel").toggle("fast");
-	return false;
-    });
-    $('#tabr1').bind("ready",geoLocateAttempt());
-});
-
 var map;
 var geocoder;
 var directionDisplay;
 var directionsService = new google.maps.DirectionsService();
 var styles;
 var minZoomLevel = 10;
-var maxZoomLevel = 19;
+
 var strictBounds = new google.maps.LatLngBounds(
     new google.maps.LatLng(40.473069,-74.31015), 
     new google.maps.LatLng(40.922852,-73.693542)
 );
 
-//W3C Geolocation
-function geoLocateAttempt(){
-    if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-    }
-    else {
-	alert('It seems like Geolocation, which is required for this page, is not enabled in your browser. Please use a browser which supports it.');
-    }
-}
-
-function successFunction(position) {
-    var lat = position.coords.latitude;
-    var long = position.coords.longitude;
-    alert('Your latitude is :'+lat+' and longitude is '+long);
-}
-
-function errorFunction(position) {
-    alert('Could not automatically detect location! Please enter manually');
-}
-//END GEOLOCATION
-
-//GOOGLE API
 function initialize() {
-    var browser = BrowserDetect.browser;
-    var bversion = BrowserDetect.version;
-    if(browser != 'Chrome' && browser != 'Safari'){
-	alert('Please use Google Chrome or Safari');
-    }
-    else{
-	if(browser = 'Chrome'){
-	    if(bversion < 10){
-		var str = "Please update Chrome to 10 or higher for best results\nYour current version is "+bversion;
-		alert(str);
-	    }
-	}
-	if(browser = 'Safari'){
-	    if(bversion < 5.1){
-		var str = "Please update Safair to 5.1 or higher for best resulsts\nYour current version is "+bversion;
-	    }
-	}
-    }
     geocoder = new google.maps.Geocoder();
     directionsDisplay = new google.maps.DirectionsRenderer();
     var latlng = new google.maps.LatLng(40.720461,-74.013519);
@@ -70,11 +22,12 @@ function initialize() {
     map = new google.maps.Map(document.getElementById('map_canvas'),
 			      mapOptions);
     directionsDisplay.setMap(map);
-    
+
     google.maps.event.addListener(map, 'zoom_changed', function() {
 	if(map.zoom < minZoomLevel) map.setZoom(minZoomLevel);
-	if(map.zoom > maxZoomLevel) map.setZoom(maxZoomLevel);
     });
+
+    changeMS('aliens');
     google.maps.event.addListener(map, 'dragend', function() {
 	if (strictBounds.contains(map.getCenter())) return;
 	var c = map.getCenter(),
@@ -84,57 +37,14 @@ function initialize() {
         maxY = strictBounds.getNorthEast().lat(),
         minX = strictBounds.getSouthWest().lng(),
         minY = strictBounds.getSouthWest().lat();
+	
 	if (x < minX) x = minX;
 	if (x > maxX) x = maxX;
 	if (y < minY) y = minY;
 	if (y > maxY) y = maxY;
+	
 	map.setCenter(new google.maps.LatLng(y, x));
     });
-    
-    var input = document.getElementById('target');
-    var searchBox = new google.maps.places.SearchBox(input);
-    var markers = [];
-    
-    google.maps.event.addListener(searchBox, 'places_changed', function() {
-        var places = searchBox.getPlaces();
-        for (var i = 0, marker; marker = markers[i]; i++) {
-           marker.setMap(null);
-        }
-	
-        markers = [];
-        var bounds = new google.maps.LatLngBounds();
-        for (var i = 0, place; place = places[i]; i++) {
-            var image = new google.maps.MarkerImage(
-                place.icon, new google.maps.Size(71, 71),
-                new google.maps.Point(0, 0), new google.maps.Point(17, 34),
-                new google.maps.Size(25, 25));
-	    
-            var marker = new google.maps.Marker({
-		map: map,
-		icon: image,
-		title: place.name,
-		position: place.geometry.location
-            });
-	    
-            markers.push(marker);
-	    
-            bounds.extend(place.geometry.location);
-        }
-	
-        map.fitBounds(bounds);
-    });
-    
-    google.maps.event.addListener(map, 'bounds_changed', function() {
-        var bounds = map.getBounds();
-        searchBox.setBounds(bounds);
-    });
-    
-    google.maps.event.addListener(map, 'bounds_changed', function() {
-        var bounds = map.getBounds();
-        searchBox.setBounds(bounds);
-    });
-    //var trafficLayer = new google.maps.TrafficLayer();
-    //trafficLayer.setMap(map);
 }
 
 function changeMS(str){
@@ -161,7 +71,6 @@ function changeMS(str){
     }
     map.setOptions({styles: styles});
 }
-
 function codeAddress() {
     var address = document.getElementById('address').value;
     geocoder.geocode( { 'address': address}, function(results, status) {
@@ -195,135 +104,17 @@ function calcRoute() {
 
 // END GOOGLE STUFF
 
-var BrowserDetect = {
-    init: function () {
-	this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
-	this.version = this.searchVersion(navigator.userAgent)
-	    || this.searchVersion(navigator.appVersion)
-	    || "an unknown version";
-	this.OS = this.searchString(this.dataOS) || "an unknown OS";
-    },
-    searchString: function (data) {
-	for (var i=0;i<data.length;i++)	{
-	    var dataString = data[i].string;
-	    var dataProp = data[i].prop;
-	    this.versionSearchString = data[i].versionSearch || data[i].identity;
-	    if (dataString) {
-		if (dataString.indexOf(data[i].subString) != -1)
-		    return data[i].identity;
-	    }
-	    else if (dataProp)
-		return data[i].identity;
-	}
-    },
-    searchVersion: function (dataString) {
-	var index = dataString.indexOf(this.versionSearchString);
-	if (index == -1) return;
-	return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
-    },
-    dataBrowser: [
-	{
-	    string: navigator.userAgent,
-	    subString: "Chrome",
-	    identity: "Chrome"
-	},
-	{ 	string: navigator.userAgent,
-		subString: "OmniWeb",
-		versionSearch: "OmniWeb/",
-		identity: "OmniWeb"
-	},
-	{
-	    string: navigator.vendor,
-	    subString: "Apple",
-	    identity: "Safari",
-	    versionSearch: "Version"
-	},
-	{
-	    prop: window.opera,
-	    identity: "Opera",
-	    versionSearch: "Version"
-	},
-	{
-	    string: navigator.vendor,
-	    subString: "iCab",
-	    identity: "iCab"
-	},
-	{
-	    string: navigator.vendor,
-	    subString: "KDE",
-	    identity: "Konqueror"
-	},
-	{
-	    string: navigator.userAgent,
-	    subString: "Firefox",
-	    identity: "Firefox"
-	},
-	{
-	    string: navigator.vendor,
-	    subString: "Camino",
-	    identity: "Camino"
-	},
-	{		// for newer Netscapes (6+)
-	    string: navigator.userAgent,
-	    subString: "Netscape",
-	    identity: "Netscape"
-	},
-	{
-	    string: navigator.userAgent,
-	    subString: "MSIE",
-	    identity: "Explorer",
-	    versionSearch: "MSIE"
-	},
-	{
-	    string: navigator.userAgent,
-	    subString: "Gecko",
-	    identity: "Mozilla",
-	    versionSearch: "rv"
-	},
-	{ 		// for older Netscapes (4-)
-	    string: navigator.userAgent,
-	    subString: "Mozilla",
-	    identity: "Netscape",
-	    versionSearch: "Mozilla"
-	}
-    ],
-    dataOS : [
-	{
-	    string: navigator.platform,
-	    subString: "Win",
-	    identity: "Windows"
-	},
-	{
-			string: navigator.platform,
-	    subString: "Mac",
-	    identity: "Mac"
-	},
-	{
-	    string: navigator.userAgent,
-	    subString: "iPhone",
-	    identity: "iPhone/iPod"
-	},
-	{
-	    string: navigator.platform,
-	    subString: "Linux",
-	    identity: "Linux"
-	}
-    ]
-    
-};
-BrowserDetect.init();
-
 function changeBGC(str){
     if(str==='aliens'){
-	$('body,html,#map_canvas').css('background','-webkit-linear-gradient(-45deg, #aebcbf 0%,#6e7774 50%,#0a0e0a 51%,#0a0809 100%)');
+	$('body,html,#map_canvas').css('background','-webkit-linear-gradient(45deg, rgba(191,210,85,1) 0%,rgba(142,185,42,1) 85%,rgba(158,203,45,1) 100%)');
     }
     else if(str==='fire'){
-	$('body,html,#map_canvas').css('background','-webkit-linear-gradient(-45deg, #fceabb 0%,#fccd4d 50%,#f8b500 51%,#fbdf93 100%)');
+	$('body,html,#map_canvas').css('background','-webkit-linear-gradient(45deg, rgba(252,234,187,1) 0%,rgba(248,181,0,1) 6%,rgba(252,205,77,1) 50%,rgba(251,223,147,1) 100%)');
     }
     else if(str==='hurricane'){
-	$('body,html,#map_canvas').css('background','-webkit-linear-gradient(-45deg, #6db3f2 0%,#54a3ee 50%,#3690f0 51%,#1e69de 100%)');
+	$('body,html,#map_canvas').css('background','-webkit-linear-gradient(45deg, rgba(254,255,255,1) 0%,rgba(221,241,249,1) 35%,rgba(160,216,239,1) 100%)');
     }
     else{
-	$('body,html,#map_canvas').css('background','-webkit-linear-gradient(-45deg, #bfd255 0%,#8eb92a 50%,#72aa00 51%,#9ecb2d 100%)');
+	$('body,html,#map_canvas').css('background','-webkit-linear-gradient(45deg, rgba(241,111,92,1) 5%,rgba(248,80,50,1) 33%,rgba(240,47,23,1) 67%,rgba(246,41,12,1) 84%,rgba(231,56,39,1) 100%)');
     }
 }
