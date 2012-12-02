77516d4ba9fe1ef39eddc62a9c3f7bd853ce62ef
jhaggerty95 <jhaggerty95@gmail.com>
2012-12-02 14:54:41 -0500

    app.py

--- DanielPageProbablyHerHead/app.py
+++ DanielPageProbablyHerHead/app.py
@@ -3,17 +3,14 @@ import db
 
 app = Flask(__name__)
 
-#quote = wiki-proof.getQuote()
-#title = wiki-proof.getTitle()
-
 @app.route("/",methods=['GET','POST'])
 def index():
+    allAlbums = db.getImages()
     if request.method == 'POST':
-        allAlbums = db.getImages()
-        if request.form['button'] == 'Generate stuff!':
-            db.addImage('1')
+        if request.form['button'] == 'Save':
+            db.addImage("http://farm9.staticflickr.com/8070/8234964776_708125d9a6_z.jpg")
     else:
-        return render_template("index.html")
+        return render_template("index.html", allAlbums = allAlbums)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7202, debug=True)
