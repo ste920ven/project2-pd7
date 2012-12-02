241a220c609cbc4a466a02e601a76721ae98628a
chemdog <eliftw@gmail.com>
2012-12-02 14:07:02 -0500

    util.py now downloads pics and moves them to the images folder'

--- GROUP_6/util.py
+++ GROUP_6/util.py
@@ -1,7 +1,8 @@
-
 import json
 import requests
 import urllib
+import shutil
+import os
 from StringIO import StringIO
 
 #this is to login to the reddit API
@@ -24,7 +25,7 @@ r = client.post(r'http://www.reddit.com/api/login', data = user_pass_dict)
 j =  json.loads(r.text)
 client.modhash = j['json']['data']['modhash']
 
-
+#this gets the json data from when you search sandy in reddit
 url = 'http://www.reddit.com/search.json?q=sandy'
 request = urllib.urlopen(url)
 results = json.loads(request.read())
@@ -34,11 +35,13 @@ def image_crawl(num):
     i = 0
     url_list = []
     while (i < num):
+        #stringIO allows for manipulation of strings
         io = StringIO()
+        #this dumps the JSON data into a string
         json.dump(results['data']['children'][i]['data']['url'], io)
+        #this makes sure it's an imgur link that isn't an album
         if ('imgur' in io.getvalue() and '#' not in io.getvalue() and '/a/' not in io.getvalue()):
-            #url = io.getvalue()
-            #url = strip(url
+            #get rid of extra quotes 
             url_list.append(io.getvalue().strip('"'))
         i = i + 1    
     
@@ -49,10 +52,23 @@ def get_image_url(url):
     if ('i.imgur' in url):
         return url, url[-9:]
     else:
-        #this takes
+        #this returns the parameters for retrieving an image.
         return 'http://www.i.imgur.com/'+url[-5:]+'.jpg', url[-5:]+'.jpg'
         
+def save_image(url):
+    
+    #create image space
+    image = urllib.URLopener()
+    url,filename = get_image_url(url)
+    #download file into home directory.
+    image.retrieve(url,filename)
+    
+    src = os.getcwd()+'/'+filename
+    dst = os.getcwd()+'/images/'
+    shutil.move(src,dst)
 
+
+save_image('http://i.imgur.com/x6nCC.jpg')
     
 #print results['data']['children'][0]['data']['url']
 
