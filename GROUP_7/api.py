b84d24ceb79f7c21e90b78e3c49655e06a0e3d87
Sarah Duncan <duncanbdunkin@gmail.com>
2012-12-02 00:15:59 -0500

    all facility names work

--- GROUP_7/api.py
+++ GROUP_7/api.py
@@ -42,7 +42,40 @@ def isPOI(origins):
         return "Yes"
     else:
         return "No"
+
+def getFacilityList(origin, category, mode): 
+    facilityList_url = "http://www.nyc.gov/portal/apps/311_contentapi/facilities/%s.json"%(category)
+    facilityList_request = urllib.urlopen(facilityList_url)
+    facilityList_results = json.loads(facilityList_request.read())
+    facilities = [] 
+    for item in facilityList_results:
+        facilities.append(item["facility_name"])
+    facilities.sort()
+    for facility in facilities:
+        print "\n", facility
+
     
+        """
+        #fac_in_order = []
+        #dist_in_order = []
+        for facility in facilities:
+        #fac_add = getAddress(facility)
+        #distance = distanceFrom(origin, fac_add, mode)
+        #dist_in_order.append(distance)
+        if len(fac_in_order) == 0: 
+        fac_in_order.append(facility)
+        else:
+        if this.distance > fac_in_order[0].distance:
+        fac_in_order.append(facility)
+        else:
+        for a in fac_in_order:
+        if this.distance <= a.distance:
+        fac_in_order.insert((fac_in_order.index(a)), facility)
+        dist_in_order.sort()
+        print fac_in_order
+        print dist_in_order
+        """
+        
 
 original = "345 Chambers Street"
 destination = "97 Warren Street"
@@ -54,4 +87,5 @@ getLatLong(original)
 distanceFrom(original, destination, m)
 durationTo(original, destination, m)
 print "\n Is %s a POI? : %s" %(original, isPOI(original))
-print "\n Is %s a POI? : %s" %(poi, isPOI(poi))
+print "\n Is %s a POI? : %s \n" %(poi, isPOI(poi))
+getFacilityList(original, "Hospital", m);
