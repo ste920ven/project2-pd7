00d498b1e7e1941d10f32a11b698d8688a9204e7
Shreya K <satishkalva@Satishs-MacBook-Pro.local>
2012-12-02 00:46:15 -0500

    updated flickr-proof

--- DanielPageProbablyHerHead/flickr-proof.py
+++ DanielPageProbablyHerHead/flickr-proof.py
@@ -8,19 +8,21 @@ api_secret = "f595faa22722ff96"
 #Gets a list of the URLs of the most recent public photos uploaded to Flickr (I thought #this would be the best way to access random photos)
 
 def getRecentPhotos(recentPhotoList):
+	photoURLs = []
 	for photo in recentPhotoList:
-	   print "http://" str(farm(photo['farm'])) + ".staticflickr.com/" + str(photo['server']) + "/" + str(photo['id'])+ "_" + str(photo['secret']) + ".jpg"
+	   photoURLs.append("http://" + str(farm(photo['farm'])) + ".staticflickr.com/" + str(photo['server']) + "/" + str(photo['id'])+ "_" + str(photo['secret']) + ".jpg")
+	return photoURLs
 
 #Picks a random photo URL, given a list of photo URLs
-def pickRandomPhoto(photoList):
-	randNum = randint(0,len(photoList)-1)
-	print photoList[randNum]
+def pickRandomPhoto(URLList):
+	randNum = randint(0,len(URLListList)-1)
+	print URLList[randNum]
 
 def main():
 	print "Random Photo URL:"
-	flickr = flickrapi.FlickrAPI(api_key)
-	recentPics = flickr.photos_getRecent()
-	pickRandomPhoto(getRecentPhotos(recentPics))
+	flickr = flickrapi.FlickrApi(api_key = "c190109eeac99e777f3246f6da0f263a", format = "json")	
+	photoList = flickr.photos_getRecent()
+	pickRandomPhoto(getRecentPhotos(photoList))
 
 if __name__ == '__main__':
 	main()
