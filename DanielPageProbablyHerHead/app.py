e8002885a1a6aa958a980cea157cce3fe1098a6d
jhaggerty95 <jhaggerty95@gmail.com>
2012-12-02 17:39:51 -0500

    a lil more app

--- DanielPageProbablyHerHead/app.py
+++ DanielPageProbablyHerHead/app.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 def index():
     allAlbums = db.getImages()
     if request.method == 'POST':
-        if request.form['button'] == 'Generate':
-            print "Hello"
+        button = request.form['button']
+        if button == 'Save':
             db.addImage("http://farm9.staticflickr.com/8070/8234964776_708125d9a6_z.jpg")
+            return render_template("index.html", allAlbums = allAlbums)
+       if button == 'Edit':
+            return render_template("index.html", allAlbums = allAlbums)
+       if button == 'Generate':
             return render_template("index.html", allAlbums = allAlbums)
     else:
         return render_template("index.html", allAlbums = allAlbums)
