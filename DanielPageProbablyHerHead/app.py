d78dcbc2b48fb029dda635fac5e4d089141f6252
jhaggerty95 <jhaggerty95@gmail.com>
2012-12-02 18:50:25 -0500

    imported api instead

--- DanielPageProbablyHerHead/app.py
+++ DanielPageProbablyHerHead/app.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import db
-#import api
-from random import randint
-import urllib2
-import simplejson
-import flickrapi
+import api
 
 app = Flask(__name__)
 
@@ -12,33 +8,6 @@ app = Flask(__name__)
 def index():
     allAlbums = db.getImages()
     if request.method == 'POST':
-         def newAlbumPicture():
-             flickr = flickrapi.FlickrAPI(api_key = "c190109eeac99e777f3246f6da0f263a", format = "json")
-             randNum = randint(0, 9)
-             pickedPhotos = []
-             if (randNum <= 5):
-                 pickedPhotos = flickr.interestingness_getList()
-             else: 
-                 pickedPhotos = flickr.photos_getRecent()
-             idIndices = []
-             k = 0
-             i = str(pickedPhotos).find('id')
-             while k <= 1000:
-                 idIndices.append(i)
-                 i = str(pickedPhotos).find('id', i + 200)
-                 k = k + 1
-             randNum = randint(0, len(idIndices) - 1)
-             start = idIndices[randNum]
-             id = str(pickedPhotos)[start + 5: start + 15]
-             secret = str(pickedPhotos)[start + 52: start + 62]
-             server = str(pickedPhotos)[start + 75: start + 79]
-             farm = str(pickedPhotos)[start + 89: start + 90]
-             if (farm.isdigit() and server.isdigit() and id.isdigit()): 
-                 URL = "http://farm" + str(farm) + ".staticflickr.com/" + str(server) + "/" + str(id) + "_" + str(secret) + "_z" + ".jpg"
-                 return URL
-             else:
-                 return newAlbumPicture()
-            
          button = request.form['button']
          if button == 'Save':
              db.addImage("http://farm9.staticflickr.com/8070/8234964776_708125d9a6_z.jpg")
@@ -46,7 +15,7 @@ def index():
          if button == 'Edit':
              return render_template("index.html", allAlbums = allAlbums)
          if button == 'Generate':
-             source = newAlbumPicture()
+             source = api.newAlbumPicture()
              return render_template("index.html", allAlbums = allAlbums, source = source)
     else:
         return render_template("index.html", allAlbums = allAlbums)
