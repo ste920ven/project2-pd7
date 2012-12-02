4aaedf4a138729281361b88d10bf3bf23d2b9f17
Shan Shi <a455898334@gmail.com>
2012-12-02 08:23:36 -0500

    organized a little

---
+++ DanielPageProbablyHerHead/static/jovoscropt.js
@@ -0,0 +1,15 @@
+<!-- this sets up the slideything --!>
+    $(function(){
+        $('.slide-out-div').tabSlideOut({
+            tabHandle: '.handle',                     //class of the element that will become your tab
+            pathToTabImage: '../static/arrows.jpg', 			  //path to the image for the tab //Optionally can be set using css
+            imageHeight: '122px',                     //height of tab image           //Optionally can be set using css
+            imageWidth: '40px',                       //width of tab image            //Optionally can be set using css
+            tabLocation: 'left',                      //side of screen where tab lives, top, right, bottom, or left
+            speed: 300,                               //speed of animation
+            action: 'hover',                          //options: 'click' or 'hover', action to trigger animation
+            topPos: '0px',                          //position from the top/ use if tabLocation is left or right
+            leftPos: '20px',                          //position from left/ use if tabLocation is bottom or top
+            fixedPosition: false                      //options: true makes it stick(fixed position) on scroll
+        });
+    });
