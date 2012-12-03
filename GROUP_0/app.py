9547037d7e8ebec925bcf55ffae3a128e9fee9b2
mengdilin <mengdilin95@gmail.com>
2012-12-02 18:55:56 -0800

    Update GROUP_0/app.py

--- GROUP_0/app.py
+++ GROUP_0/app.py
@@ -1,4 +1,4 @@
-ffrom flask import Flask
+from flask import Flask
 from flask import request
 from flask import render_template
 from flask import url_for,redirect,flash
@@ -63,7 +63,7 @@ def album(album=""):
             database.addAlbumrating(username,name,data_album[name]["artist"],rating_value,comment)
             return render_template("album.html", albums=data_album.keys(),images_album=images_album)
         if button == "go to songs":
-            return redirect(url_for("song"))
+            return redirect(url_for("login"))
 
 @app.route("/hello/song/<song>",methods=['GET','POST'])
 def song(song=""):
@@ -84,7 +84,7 @@ def song(song=""):
             database.addSongRating(username,name,data_song[name]["artist"],rating_value,comment)
             return render_template("song.html", songs=data_song.keys())
         if button == "go to albums":
-           return redirect(url_for("album"))
+           return redirect(url_for("login"))
 
 if __name__=="__main__":
     app.debug=True
