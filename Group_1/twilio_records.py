fbb26fbe10e6cf0e2d4a1676159d3bfecf17823e
Steven <steven@ubuntu.ubuntu-domain>
2012-12-02 21:44:11 -0500

    updated

--- Group_1/twilio_records.py
+++ Group_1/twilio_records.py
@@ -1,17 +1,17 @@
 from twilio.rest import TwilioRestClient
-from bs4 import BeautifulSoup
 import json
 import urllib
 
+account = 'ACf662305afa76bba6f077405676222615'
+token = '73452f5f4a6931a17ce11c93d6b6b473'
+client = TwilioRestClient(account,token)
 
 def get_records(number):
-    rq = urllib.urlopen("https://api.twilio.com/2010-04-01/Accounts/ACf662305afa76bba6f077405676222615/SMS/Messages.json")
-    re = json.loads(rq.read())
+    messages = client.sms.messages.list()
     sent={}
-    for sms in re["sms_messages"]:
-        if sms["status"] == "sent" and sms["to"] == number:
-            key = sms['body'][6:sms['body'].find('\n')]
-            sent[key]=sms['body']                               
+    for sms in messages:
+        if str(sms.status)=="sent" and int(sms.to) == number:
+            key = sms.body[6:sms.body.find('\n')]
+            sent[key]=sms.body                               
     return sent
 
-    
