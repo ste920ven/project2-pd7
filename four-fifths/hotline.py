896c59fe1e77598a2616d87aa14bdbb2b281e42f
kajabla <jskestrel@gmail.com>
2012-12-04 13:15:36 -0500

    MTA texting

--- four-fifths/hotline.py
+++ four-fifths/hotline.py
@@ -10,20 +10,31 @@ def text():
     data = extractor.loadStuySite()
     schedule = extractor.getSchedule(data[1], data[2])
     bellDay = extractor.getBellDay(schedule)
+    delays = MTAService.getDelays(MTAService.getSubways())
     resp = twiml.Response()
+    message = ""
     if bellDay == "Closed" :
-        message = "School is closed today."
+        message += "School is closed today."
     elif bellDay == "Weekend" :
-        message = "It's a weekend. There is no school today."
+        message += "It's a weekend. There is no school today."
     elif bellDay == "Unknown" :
-        message = "We don't have today's schedule. How embarrassing."
+        message += "We don't have today's schedule. How embarrassing."
     else :
         #'a' or 'an' depending on the next word:
         #B1/B2 ('a') or A1/A2/Unknown ('an')
         gymDay = extractor.getGymDay(schedule)
         if gymDay[0] == "B" : article = "a"
         else : article = "an"
-        message = "Today is a %s schedule. Today is %s %s day."%(bellDay, article, gymDay)
+        message += "Today is a %s schedule. Today is %s %s day."%(bellDay, article, gymDay)
+    #MTA delays!
+    if delays :
+        message += " There are delays on the" + delays[0]
+        #account for more than one line with delays
+        if len(delays) > 1 :
+            for i in xrange (len(delays)-1):
+                message += ", %s"%(delays[i+1])
+    else :
+        message += " There are no delays!"
     resp.sms(message)
     return str(resp)
 
