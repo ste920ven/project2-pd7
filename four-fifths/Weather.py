4f3ec70e21ba59424e16bde0b1b7292f4ad267ff
Zachary <Oneman2feet@gmail.com>
2012-12-04 15:09:10 -0500

    weather updated

--- four-fifths/Weather.py
+++ four-fifths/Weather.py
@@ -1,3 +1,57 @@
+"""
+
+0	tornado
+1	tropical storm
+2	hurricane
+3	severe thunderstorms
+4	thunderstorms
+5	mixed rain and snow
+6	mixed rain and sleet
+7	mixed snow and sleet
+8	freezing drizzle
+9	drizzle
+10	freezing rain
+11	showers
+12	showers
+13	snow flurries
+14	light snow showers
+15	blowing snow
+16	snow
+17	hail
+18	sleet
+19	dust
+20	foggy
+21	haze
+22	smoky
+23	blustery
+24	windy
+25	cold
+26	cloudy
+27	mostly cloudy (night)
+28	mostly cloudy (day)
+29	partly cloudy (night)
+30	partly cloudy (day)
+31	clear (night)
+32	sunny
+33	fair (night)
+34	fair (day)
+35	mixed rain and hail
+36	hot
+37	isolated thunderstorms
+38	scattered thunderstorms
+39	scattered thunderstorms
+40	scattered showers
+41	heavy snow
+42	scattered snow showers
+43	heavy snow
+44	partly cloudy
+45	thundershowers
+46	snow showers
+47	isolated thundershowers
+3200	not available
+
+"""
+
 import urllib2,json
 
 def getTemp():
@@ -33,54 +87,8 @@ def getForecast():
     return int(x)
 
 def getForecastString(code):
-    d1 = ["tornado",
-          "tropical storm", 
-          'hurricane',
-          "severe thunderstorms",
-          "thunderstorms",
-          "mixed rain and snow",
-          "mixed rain and sleet",
-          "mixed snow and sleet",
-          "freezing drizzle",
-          "drizzle",
-          "freezing rain",
-          "showers",
-          "showers",
-          "snow flurries",
-          "light snow showers",
-          "blowing snow",
-          "snow",
-          "hail",
-          "sleet",
-          "dust",
-          "foggy",
-          "haze",
-          "smoky",
-          "blustery",
-          "windy",
-          "cold",
-          "cloudy",
-          "mostly cloudy (night)",
-          "mostly cloudy (day)",
-          "partly cloudy (night)",
-          "partly cloudy (day)",
-          "clear (night)",
-          "sunny",
-          "fair (night)",
-          "fair (day)",
-          "mixed rain and hail",
-          "hot",
-          "isolated thunderstorms",
-          "scattered thunderstorms",
-          "scattered thunderstorms",
-          "scattered showers",
-          "heavy snow",
-          "scattered snow showers",
-          "heavy snow",
-          "partly cloudy",
-          "thundershowers",
-          "snow showers",
-          "isolated thundershowers"]
+    d1 = ["tornado","tropical storm","hurricane","severe thunderstorms","thunderstorms","mixed rain and snow","mixed rain and sleet","mixed snow and sleet","freezing drizzle","drizzle","freezing rain","showers","showers","snow flurries","light snow showers","blowing snow","snow","hail","sleet","dust","foggy","haze","smoky","blustery","windy","cold","cloudy","mostly cloudy (night)","mostly cloudy (day)","partly cloudy (night)","partly cloudy (day)","clear (night)","sunny","fair (night)","fair (day)","mixed rain and hail","hot","isolated thunderstorms","scattered thunderstorms","scattered thunderstorms","scattered showers","heavy snow","scattered snow showers","heavy snow","partly cloudy","thundershowers","snow showers","isolated thundershowers"]
+    if code==3200: return "not available"
     return d1[code]
 
 
