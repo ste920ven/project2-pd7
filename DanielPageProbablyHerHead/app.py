bc3904ba2a0dce0f530f09ee4250f68f5ee45582
jhaggerty95 <jhaggerty95@gmail.com>
2012-12-04 01:47:57 -0500

    Pretty

--- DanielPageProbablyHerHead/app.py
+++ DanielPageProbablyHerHead/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import db
 import api
-import xml
+from random import randint
 
 app = Flask(__name__)
 
@@ -10,11 +10,14 @@ app = Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 def index():
     allAlbums = db.getImages()
+    def randomize():
+        randNum = randint(0, 999)
+        return allAlbums[randNum]
     if request.method == 'POST':                
         global source
         button = request.form['button']
         if button == 'Generate':
-            source = api.newAlbumPicture()
+            source = randomize()
             band = api.newArtistName()
             name = api.newAlbumName()
             return render_template("index1.html", allAlbums = allAlbums, source = source, band = band, name = name)
