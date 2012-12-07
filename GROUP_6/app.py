c70edabdc25a990f352432c132a774c80350317d
alexzxz <wu.allen154@gmail.com>
2012-12-07 02:13:07 -0500

    Update GROUP_6/app.py

--- GROUP_6/app.py
+++ GROUP_6/app.py
@@ -18,7 +18,7 @@ def home():
 		return render_template("gallery.html",url_list = url_list)
 	else:	
 		url = request.form['hidSrc']
-		db.addPicture(self, url)
+		db.addPicture(url)
 		return redirect(url_for("/" ))
 
 @app.route('/slideshow/<tag>', methods=['GET', 'POST'])
@@ -26,8 +26,8 @@ def slide():
 	global tag
 	global picurl
 	if request.method=="GET":
-		taglist = db.getTaglist(self)
-		piclist = db.getPics(self,tag)
+		taglist = db.getTaglist()
+		piclist = db.getPics(tag)
 		return render_template("slide.html", taglist = taglist, piclist =piclist)
 	else:	
 		url = request.form['hidSrc']
@@ -38,24 +38,24 @@ def slide():
 def image():
 	import db
 	if request.method=="GET":
-		taglist = db.getTaglist(self)
-		tags = db.getTags(self, picurl)
+		taglist = db.getTaglist()
+		tags = db.getTags(picurl)
 		url = request.form['hidSrc']
-		commentlist = db.getComments(self, picurl)
+		commentlist = db.getComments(picurl)
 		return render_template("image.html", taglist = taglist, tags =tags, commentlist = commentlist, pic = url)
 	else:	
 		button = request.form['button']
 				
 		if button=="submit":
 			aComment = request.form['comment']
-			db.addComment(self, picurl,aComment)
+			db.addComment(picurl,aComment)
 		elif button == "submitnewtag":
 			if request.form['Addnewtag']:
 				aTag = request.form['Addnewtag']
-				db.addTag(self, picurl,aTag)
+				db.addTag(picurl,aTag)
 			else:
 				aTag =  request.form['select1']
-				db.addTag(self, picurl,aTag)
+				db.addTag(picurl,aTag)
 		
 		 	 	
 if __name__=="__main__":
